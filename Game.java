4c50317b8e7e2e1a4c60fbc5bfc2b2bd8a86bd4e
corvusbkgk <corvus57@gmail.com>
2013-12-10 21:26:20 +0300

    Removed rotatiton, added proper view angle

--- Game.java
+++ Game.java
@@ -5,11 +5,16 @@ import org.lwjgl.opengl.GL11;
 
 public class Game {
     World world;
+    public static final int MAX_X=30;
+    public static final int MAX_Y=30;
+    public static final int MAX_Z=10;
+    public static final double SCALE=0.7;
+
 
 	public static void main(String[] args) {
-			Game game = new Game();
-            game.world = new World(10,10,10);
-		    game.start();
+		Game game = new Game();
+		game.world = new World(MAX_X,MAX_Y,MAX_Z);
+		game.start();
 	}
 
 	public void start() {
@@ -27,11 +32,13 @@ public class Game {
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
 		GL11.glOrtho(20, -20, 15f, -15f, 20, -20);
+		GL11.glRotated(135, 1.0, 0.0, 0.0);
+		GL11.glRotated(45, 0.0, 0.0, 1.0);
+                GL11.glScaled(SCALE, SCALE, SCALE);
+                GL11.glTranslated(-MAX_X/2, -MAX_Y/2, -MAX_Z/2);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
-		GL11.glRotatef(-50.0f, 1.0f, 0.0f, 0.0f);
 
 		while (!Display.isCloseRequested()) {
-			GL11.glRotatef(0.5f, 0.0f, 0.0f, 1.0f);
 			// Clear the screen and depth buffer
 			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
