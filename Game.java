2ed523b76b4aaa2de366731f9e44f4b2bfbb6a37
Yampolskiy Anton <corvus57@gmail.com>
2013-12-10 17:41:15 +0400

    Game: Now with more floating cube.

--- Game.java
+++ Game.java
@@ -22,7 +22,67 @@ public class Game {
 		}
 		System.out.println("Display created. OpenGL version: " + GL11.glGetString(GL11.GL_VERSION));
 
+		// init OpenGL
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(1, -1, 0.75f, -0.75f, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glRotatef(50.0f, 1.0f, 1.0f, 1.0f);
+
 		while (!Display.isCloseRequested()) {
+			GL11.glRotatef(0.5f, 1.0f, 0.1f, 0.0f);
+			// Clear the screen and depth buffer
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+			// draw quad
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glColor3f(0.6f,0.45f,0.25f); GL11.glVertex3f(0.0f,0.0f,0.0f);
+			GL11.glColor3f(0.6f,0.45f,0.25f); GL11.glVertex3f(0.1f,0.0f,0.0f);
+			GL11.glColor3f(0.6f,0.45f,0.25f); GL11.glVertex3f(0.1f,0.1f,0.0f);
+			GL11.glColor3f(0.6f,0.45f,0.25f); GL11.glVertex3f(0.0f,0.1f,0.0f);
+			GL11.glEnd();
+
+			// draw quad
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glColor3f(0.7f,0.45f,0.25f); GL11.glVertex3f(0.0f,0.0f,0.0f);
+			GL11.glColor3f(0.7f,0.45f,0.25f); GL11.glVertex3f(0.0f,0.1f,0.0f);
+			GL11.glColor3f(0.7f,0.45f,0.25f); GL11.glVertex3f(0.0f,0.1f,0.1f);
+			GL11.glColor3f(0.7f,0.45f,0.25f); GL11.glVertex3f(0.0f,0.0f,0.1f);
+			GL11.glEnd();
+
+			// draw quad
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glColor3f(0.6f,0.55f,0.25f); GL11.glVertex3f(0.0f,0.0f,0.0f);
+			GL11.glColor3f(0.6f,0.55f,0.25f); GL11.glVertex3f(0.1f,0.0f,0.0f);
+			GL11.glColor3f(0.6f,0.55f,0.25f); GL11.glVertex3f(0.1f,0.0f,0.1f);
+			GL11.glColor3f(0.6f,0.55f,0.25f); GL11.glVertex3f(0.0f,0.0f,0.1f);
+			GL11.glEnd();
+
+			// draw quad
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glColor3f(0.6f,0.35f,0.25f); GL11.glVertex3f(0.1f,0.0f,0.0f);
+			GL11.glColor3f(0.6f,0.35f,0.25f); GL11.glVertex3f(0.1f,0.1f,0.0f);
+			GL11.glColor3f(0.6f,0.35f,0.25f); GL11.glVertex3f(0.1f,0.1f,0.1f);
+			GL11.glColor3f(0.6f,0.35f,0.25f); GL11.glVertex3f(0.1f,0.0f,0.1f);
+			GL11.glEnd();
+
+			// draw quad
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glColor3f(0.0f,1.0f,0.0f); GL11.glVertex3f(0.0f,0.0f,0.1f);
+			GL11.glColor3f(0.0f,1.0f,0.0f); GL11.glVertex3f(0.1f,0.0f,0.1f);
+			GL11.glColor3f(0.0f,1.0f,0.0f); GL11.glVertex3f(0.1f,0.1f,0.1f);
+			GL11.glColor3f(0.0f,1.0f,0.0f); GL11.glVertex3f(0.0f,0.1f,0.1f);
+			GL11.glEnd();
+
+			// draw quad
+			GL11.glBegin(GL11.GL_QUADS);
+			GL11.glColor3f(0.6f,0.45f,0.15f); GL11.glVertex3f(0.0f,0.1f,0.0f);
+			GL11.glColor3f(0.6f,0.45f,0.15f); GL11.glVertex3f(0.1f,0.1f,0.0f);
+			GL11.glColor3f(0.6f,0.45f,0.15f); GL11.glVertex3f(0.1f,0.1f,0.1f);
+			GL11.glColor3f(0.6f,0.45f,0.15f); GL11.glVertex3f(0.0f,0.1f,0.1f);
+			GL11.glEnd();
+
 			Display.update();
 		}
 
