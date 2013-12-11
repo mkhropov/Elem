b7ef5f8e574e101096788a8669dabf89c13a52f9
corvusbkgk <corvus57@gmail.com>
2013-12-11 10:56:17 +0300

    Game: added current layer view (Z and X to navigate)

--- Game.java
+++ Game.java
@@ -2,22 +2,57 @@ import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import java.nio.FloatBuffer;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
 public class Game {
-    World world;
-    public static final int MAX_X=30;
-    public static final int MAX_Y=30;
-    public static final int MAX_Z=10;
-    public static final double SCALE=0.7;
+	World world;
+	public static final int MAX_X=30;
+	public static final int MAX_Y=30;
+	public static final int MAX_Z=10;
+	public static final double SCALE=0.7;
+	int current_layer=MAX_Z-1;
 
 
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.world = new World(MAX_X,MAX_Y,MAX_Z);
 		game.start();
+	}
+
+	public void pollInput() {
+		if (Mouse.isButtonDown(0)) {
+			int x = Mouse.getX();
+			int y = Mouse.getY();
+
+			System.out.println("MOUSE DOWN @ X: " + x + " Y: " + y);
+		}
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+			System.out.println("SPACE KEY IS DOWN");
+		}
+
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState()) { 
+				// Key pressed
+				if (Keyboard.getEventKey() == Keyboard.KEY_Z) {
+					if (this.current_layer>0) this.current_layer--;
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_X) {
+					if (this.current_layer<(MAX_Z-1)) this.current_layer++;
+				}
+			} else {
+				// Key released
+				if (Keyboard.getEventKey() == Keyboard.KEY_Z) {
+					//Whatever
+				}
+				if (Keyboard.getEventKey() == Keyboard.KEY_X) {
+				}
+			}
+		}
 	}
 
 	public void start() {
@@ -64,17 +99,28 @@ public class Game {
 		buffer.clear();
 		buffer.put(light_position); buffer.flip();
 		GL11.glLight(GL11.GL_LIGHT0, GL11.GL_POSITION, buffer);
+		
+		float light_ambient1[] = { 0.3f, 0.3f, 0.3f, 1.0f };
+
+		buffer.put(light_ambient1); buffer.flip();
+		GL11.glLight(GL11.GL_LIGHT1, GL11.GL_AMBIENT, buffer);
 
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 
 		while (!Display.isCloseRequested()) {
+			this.pollInput();
 			// Clear the screen and depth buffer
 			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
-			for (int i=0; i<world.xsize; i++)
-				for (int j=0; j<world.ysize; j++)
-					for (int k=0; k<world.zsize; k++) {
+			for (int k=0; k<=this.current_layer; k++) {
+				if (k==this.current_layer) {
+					GL11.glEnable(GL11.GL_LIGHT1);
+				}
+				for (int i=0; i<world.xsize; i++)
+					for (int j=0; j<world.ysize; j++) {
 						world.blockArray[i][j][k].Draw();
+				}
+				GL11.glDisable(GL11.GL_LIGHT1);
 			}
 
 			Display.update();
