46efe7bd3fb8250673ba3e885c875520a5847b42
mkhropov <mkhropov@gmail.com>
2013-12-18 17:58:55 +0400

    geomorph: fix BiomeRough generating bug
    Signed-off-by: mkhropov <mkhropov@gmail.com>

--- geomorph/BiomeRough.java
+++ geomorph/BiomeRough.java
@@ -17,7 +17,7 @@ public class BiomeRough extends Biome {
         Stratum s;
         s = new Stratum(4*w.xsize, 4*w.ysize, 2);
         s.drop(w.material[0], w, -w.xsize, -w.ysize);
-        for (int i=0; i<2*w.xsize/3; ++i){
+        for (int i=0; i<2*w.zsize/3; ++i){
             s = new Stratum(w.xsize/2, w.ysize/2, 1);
             s.drop(w.material[0], w,
                     gen.nextInt(3*w.xsize/2)-w.xsize/2,
@@ -25,7 +25,7 @@ public class BiomeRough extends Biome {
         }
         s = new Stratum(4*w.xsize, 4*w.ysize, 2);
         s.drop(w.material[1], w, -w.xsize, -w.ysize);
-        for (int i=0; i<w.xsize/2; ++i){
+        for (int i=0; i<w.zsize/2; ++i){
             s = new Stratum(w.xsize/2, w.ysize/2, 1);
             s.drop(w.material[1], w,
                     gen.nextInt(3*w.xsize/2)-w.xsize/2,
