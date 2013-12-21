06dcfad4cf656312631732a8b0b88c8104b107d9
mkhropov <mkhropov@gmail.com>
2013-12-22 01:52:57 +0400

    geomorph: new morph type - Wavefold.
    Signed-off-by: mkhropov <mkhropov@gmail.com>

--- geomorph/BiomePlains.java
+++ geomorph/BiomePlains.java
@@ -19,7 +19,7 @@ public class BiomePlains extends Biome {
 		gen = new Random((new Date()).getTime());
 		s = new Stratum(4*w.xsize, 4*w.ysize, 1);
 		s.drop(w.material[3], w, -w.xsize, -w.ysize);
-		for (i=0; i<2*w.zsize/3;){
+		for (i=0; i<w.zsize/2;){
 			k = gen.nextInt(2)+1; i+=k;
             s = new Stratum(4*w.xsize, 4*w.ysize, k);
             s.drop(w.material[gen.nextInt(2)*4], w,
@@ -29,12 +29,17 @@ public class BiomePlains extends Biome {
 		s = new Stratum(4*w.xsize, 4*w.ysize, 2);
         s.drop(w.material[1], w, -w.xsize, -w.ysize);
 		Front f; Point p;
+		f = new Front(new Point(w.xsize/2, 0, w.zsize),
+                            new Point(w.xsize/2, w.ysize, w.zsize),
+                            0.d);
+		Wavefold wmorph = new Wavefold(f, 15, 30, -3);
+		applyMorph(wmorph, w);
 		f = new Front(new Point(3*w.xsize/4, w.ysize/4, w.zsize),
                             new Point(w.xsize/4, 3*w.ysize/4, w.zsize),
                             0.d);
 		Linefold lmorph = new Linefold(f, 15, 30, -3);
 		applyMorph(lmorph, w);
- /*       f = new Front(new Point(3*w.xsize/4, w.ysize/4, w.zsize),
+        f = new Front(new Point(3*w.xsize/4, w.ysize/4, w.zsize),
                             new Point(w.xsize/4, 3*w.ysize/4, w.zsize),
                             -0.3d);
 		Slipfault smorph = new Slipfault(f, 20, 30, 4);
@@ -42,7 +47,7 @@ public class BiomePlains extends Biome {
 		p = new Point(w.xsize/2, w.ysize/2, w.zsize);
 		Pointfold pmorph = new Pointfold(p, 10, 30, -4);
 		applyMorph(pmorph, w);
-		f = new Front(new Point(0, 0, w.zsize),
+	/*	f = new Front(new Point(0, 0, w.zsize),
                             new Point(w.xsize, w.ysize, w.zsize),
                             0.d);
         smorph = new Slipfault(f, 20, 30, 3);
---
+++ geomorph/Wavefold.java
@@ -0,0 +1,48 @@
+package geomorph;
+
+import stereometry.*;
+/* Wavefold is a fold-type geomorphing, when the fold
+ * is linear and vertically directed, and goes both up and down
+ */
+
+public class Wavefold extends Morph {
+    Front f; // the fold line
+    double l; // morph length
+	double h; //maximum depth of morph applying
+    double d; // depth the plate has dropped down/moved up in the centre
+
+    public Wavefold(Front f, double l, double h, double d){
+        this.f = f;
+        this.l = l;
+        this.h = h;
+        this.d = d;
+    }
+
+
+// for now Image == Preimage. Mostly - not counting bottom (d)
+	@Override
+    public final boolean inImage(Point p){
+		return  (p.z >= this.f.p1.z-h+d)    &&
+				(f.isInside(p))          &&
+				(f.distProj(p) <= l);
+    }
+
+	@Override
+    public final Point preimage(Point p){
+        if (!inImage(p)){
+            return p;
+        } 
+        Point pi = new Point(p);
+        double r = this.f.distProj(pi);
+		double sig = (f.isUp(pi))?(-1.):(1.);
+        if(r<l){
+			double t = Math.abs(Math.sin(Math.PI*r/(l*2))+
+								Math.signum(Math.PI*r/(l*2)));
+			t *= .7d*d;
+			t *= Math.sin(Math.PI*r/l);
+		    pi.z = pi.z+sig*t;
+		}
+        return pi;
+    }
+}
+
