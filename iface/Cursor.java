5f644d25686b74dcd18759356dcee120eee95a7d
mkhropov <mkhropov@gmail.com>
2014-01-30 17:27:21 +0400

    iface: make 2D cursor a bit prettier
    Signed-off-by: mkhropov <mkhropov@gmail.com>

--- iface/Cursor.java
+++ iface/Cursor.java
@@ -41,6 +41,10 @@ public class Cursor {
 	public void draw2d(){
 		if (state != STATE_IFACE)
 			return; //or exception
+//		GL11.glDisable(GL11.GL_LIGHT0);
+//		GL11.glColorMaterial(GL11.GL_FRONT_AND_BACK, GL11.GL_AMBIENT);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+//		GL11.Color.white.bind();
 		GL11.glEnable(GL11.GL_COLOR_MATERIAL);
 		GL11.glColor3d(1., 1., 1.);
 		GL11.glBegin(GL11.GL_TRIANGLE_FAN);
@@ -49,7 +53,17 @@ public class Cursor {
 		GL11.glVertex2d(x+7, 600-y+12);
 		GL11.glVertex2d(x, 600-y+23);
 		GL11.glEnd();
+		GL11.glColor3d(0., 0., 0.);
+		GL11.glLineWidth(2);
+		GL11.glBegin(GL11.GL_LINE_STRIP);
+		GL11.glVertex2d(x, 600-y);
+		GL11.glVertex2d(x+20, 600-y+14);
+		GL11.glVertex2d(x+7, 600-y+12);
+		GL11.glVertex2d(x, 600-y+23);
+		GL11.glVertex2d(x, 600-y);
+		GL11.glEnd();
 		GL11.glDisable(GL11.GL_COLOR_MATERIAL);
+//		GL11.glEnable(GL11.GL_LIGHT0);
 	}
 
 	public void draw3d(){
