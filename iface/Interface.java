b55c37e7af8b0d2c46c0aa5d7e6c75e88945f509
mkhropov <mkhropov@gmail.com>
2014-01-31 17:31:06 +0400

    iface: disable lighting when drawing iface.
    Signed-off-by: mkhropov <mkhropov@gmail.com>

--- iface/Interface.java
+++ iface/Interface.java
@@ -66,15 +66,19 @@ public class Interface {
 			drawOrder(player.order.get(i));
 		}
 // entering 2d drawing mode
-        GL11.glViewport(0,0,800,600);
-//        glMatrixMode(GL_MODELVIEW);
-        GL11.glMatrixMode(GL11.GL_PROJECTION);
-        GL11.glLoadIdentity();
-        GL11.glOrtho(0, 800, 600, 0, 1, -1);
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glViewport(0,0,800,600);
+//		glMatrixMode(GL_MODELVIEW);
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 800, 600, 0, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 		GL11.glDisable(GL11.GL_DEPTH_TEST);
-        GL11.glLoadIdentity();
+		GL11.glDisable(GL11.GL_LIGHTING);
+		GL11.glLoadIdentity();
 		Renderer.getInstance().resetMaterial(1.f);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
+		GL11.glEnable(GL11.GL_COLOR_MATERIAL);
+		GL11.glColor3d(1., 1., 1.);
 
 		for (int i=0; i<buttons.size(); ++i)
 			buttons.get(i).draw();
@@ -82,12 +86,14 @@ public class Interface {
 		cursor.draw2d();
 
 // and leaving it. I hope.
+		GL11.glDisable(GL11.GL_COLOR_MATERIAL);
+		GL11.glEnable(GL11.GL_LIGHTING);
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
-        GL11.glLoadIdentity();
-        GLU.gluPerspective(10.0f, 4.0f/3.0f, 1.0f, 1000.0f);
-        GL11.glMatrixMode(GL11.GL_MODELVIEW);
-        GL11.glLoadIdentity();
+		GL11.glLoadIdentity();
+		GLU.gluPerspective(10.0f, 4.0f/3.0f, 1.0f, 1000.0f);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glLoadIdentity();
 		camera.forceUpdate(0);
 	}
 
