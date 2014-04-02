179201822706949dbe12ca1f2e57fa0daf71d6ca
mkhropov <mkhropov@gmail.com>
2014-04-02 16:26:53 +0400

    item: Now boulders texture depends on destroyed block material
    Note: Need to update according texture files. It looks... strange.
    
    Signed-off-by: mkhropov <mkhropov@gmail.com>

--- item/ItemBoulder.java
+++ item/ItemBoulder.java
@@ -8,6 +8,16 @@ import world.EventHandler;
 public class ItemBoulder extends Item{
 
 	double scale;
+	static final String[] assoc;
+
+	static {
+		assoc = new String[Material.MATERIAL_MAX];
+		assoc[Material.MATERIAL_STONE] = "stone";
+		assoc[Material.MATERIAL_MARBLE] = "marble";
+		assoc[Material.MATERIAL_GRANITE] = "granite";
+		assoc[Material.MATERIAL_EARTH] = "earth";
+		assoc[Material.MATERIAL_BEDROCK] = "bedrock";
+	}
 
 	public ItemBoulder(Block b, double w, char m){
 		super(b, w);
@@ -15,7 +25,7 @@ public class ItemBoulder extends Item{
 		this.m = m;
 		this.scale = Math.cbrt(w);
 		this.mid = graphics.ModelList.getInstance().findId("boulder");
-//		this.gsid = m.gsid;
+		this.gsid = graphics.GSList.getInstance().findId(assoc[m]);
 		EventHandler.getInstance().addEntity(this);
 	}
 
@@ -25,6 +35,7 @@ public class ItemBoulder extends Item{
 		this.type = Item.TYPE_BUILDABLE;
 		this.scale = Math.cbrt(w);
 		this.mid = graphics.ModelList.getInstance().findId("boulder");
+		this.gsid = graphics.GSList.getInstance().findId(assoc[m]);
 		EventHandler.getInstance().addEntity(this);
 	}
 
@@ -34,5 +45,6 @@ public class ItemBoulder extends Item{
 		this.m = m;
 		this.scale = Math.cbrt(w);
 		this.mid = graphics.ModelList.getInstance().findId("boulder");
+		this.gsid = graphics.GSList.getInstance().findId(assoc[m]);
 	}
 }
