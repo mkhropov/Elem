77db68f9fa9033026e3f4cba54e7fdf6a5ddb957
mkhropov <mkhropov@gmail.com>
2015-01-31 23:17:18 +0300

    iface: update StatsBlock submenu
    Now it corellates with vision mode
    
    Signed-off-by: mkhropov <mkhropov@gmail.com>

--- iface/StatsBlock.java
+++ iface/StatsBlock.java
@@ -1,5 +1,6 @@
 package iface;
 
+import graphics.Renderer;
 import iface.widget.*;
 import world.Block;
 import world.World;
@@ -30,18 +31,23 @@ public class StatsBlock extends Frame {
 
 		vb.add(new Label(String.format("X: %d| Y: %d| Z: %d", b.x, b.y, b.z)));
 
-		if (World.getInstance().isFull(b.x, b.y, b.z)) {
-			vb.add(new Label(String.format("Solid block of %s",
-				core.Data.Materials.get(b.m).getName())));
-		} else if (!World.getInstance().isAir(b.x, b.y, b.z)) {
-			vb.add(new Label(String.format("Floor made of %s",
-				core.Data.Materials.get(b.m).getName())));
+		if (Interface.getInstance().player.blockKnown(b.x, b.y, b.z) ||
+			((Interface.getInstance().viewMode & Renderer.VIEW_MODE_MASK) == Renderer.VIEW_MODE_FULL)) {
+			if (World.getInstance().isFull(b.x, b.y, b.z)) {
+				vb.add(new Label(String.format("Solid block of %s",
+					core.Data.Materials.get(b.m).getName())));
+			} else if (!World.getInstance().isAir(b.x, b.y, b.z)) {
+				vb.add(new Label(String.format("Floor made of %s",
+					core.Data.Materials.get(b.m).getName())));
+			} else {
+				vb.add(new Label("Empty air"));
+			}
+			if (World.getInstance().isEmpty(b.x, b.y, b.z)) {
+				InventoryMenu im = new InventoryMenu(b.getInventory());
+				vb.add(im);
+			}
 		} else {
-			vb.add(new Label("Empty air"));
-		}
-		if (World.getInstance().isEmpty(b.x, b.y, b.z)) {
-			InventoryMenu im = new InventoryMenu(b.getInventory());
-			vb.add(im);
+			vb.add(new Label("???"));
 		}
 	}
 
